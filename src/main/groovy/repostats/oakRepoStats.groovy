b843af499fa581eead856ff294bc63766b59c26a
Johnson Ho <johnho@adobe.com>
2024-04-10 20:55:48 -0400

    Update oakRepoStats.groovy

--- src/main/groovy/repostats/oakRepoStats.groovy
+++ src/main/groovy/repostats/oakRepoStats.groovy
@@ -56,7 +56,7 @@ import static org.apache.jackrabbit.oak.commons.IOUtils.humanReadableByteCount
 @CompileStatic
 class RepoStats {
     final boolean excludeIndexStats = Boolean.getBoolean("excludeIndexStats")
-    final String rootPath = System.getProperty("rootPath", "/home/users/-/-_6ZwcLLfKFfPiRDSajN")
+    final String rootPath = System.getProperty("rootPath", "/")
 
     final static String VERSION = "1.2"
 
